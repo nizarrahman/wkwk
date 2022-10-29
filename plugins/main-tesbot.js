37f0f8df367196a0d88a466d9342525cfd1451e2
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2022-10-29 18:44:31 +0700

    Update main-tesbot.js

--- plugins/main-tesbot.js
+++ plugins/main-tesbot.js
@@ -25,7 +25,7 @@ ptt: true,// true diatas ga work, sebab dipaksa tanpa convert ;v
 contextInfo: { 
          externalAdReply: { 
          showAdAttribution: true,
- 	     sourceUrl: `${pickRandom([`https://vt.tiktok.com/ZSRBqB9q1/`, `https://www.instagram.com/itskri99`, `https://youtube.com/hokenbeusz`])}`,
+ 	     sourceUrl: `${pickRandom([`https://nizarrahman.github.io/myProfile/#`, `https://www.instagram.com/nizarrrahman.f_`])}`,
            title: 'FORYN FOUR ONLINE 🥰',  
             body: `Ada yang bisa dibantu ?`, 
            thumbnail: await (await fetch(`${pickRandom(['https://telegra.ph/file/8fcbf793da8b937a21237.jpg', 'https://telegra.ph/file/bc8746f21882e9585c79b.jpg', 'https://telegra.ph/file/cf8c6dae68158bc06731d.jpg', 'https://telegra.ph/file/29b9805b4704cda560c9b.jpg'])}`)).buffer()
